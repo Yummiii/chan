2ac2873291ede2bae0f851d9f8b2ca78f3cebefe
purrleterian <jcno307@gmail.com>
2025-04-08 20:56:23 -0300

    Adicionados os arquivos de base para a frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Forum Social</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                activePage: 0,
+                pages: [
+                    {
+                        link: { text: "Home", url: "index.html" },
+                        pageTitle: "Pagina principal",
+                        content: "",
+                    },
+                    {
+                        link: { text: "About", url: "about.html" },
+                        pageTitle: "Pagina sobre",
+                        content: "",
+                    },
+                ],
+            };
+        },
+    };
+</script>
